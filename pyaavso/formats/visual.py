7fb7df4435e71efcaa7c232ad2bdbd20d90e3860
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-28 23:28:45 +0200

    And now the test passes.

--- pyaavso/formats/visual.py
+++ pyaavso/formats/visual.py
@@ -6,5 +6,18 @@ class VisualFormatWriter(object):
     A class responsible for writing observation data in AAVSO
     `Visual File Format`_.
 
+    The API here mimics the ``csv`` module in Python standard library.
+
     .. _`Visual File Format`: http://www.aavso.org/aavso-visual-file-format
     """
+
+    def __init__(self, fp, observer_code, delimiter=',', date_format='JD', obstype='Visual'):
+        """
+        Creates the writer which will write observations into the file-like
+        object given in first parameter. The only other required parameter
+        is the official AAVSO-assigned observer code.
+        """
+        self.observer_code = observer_code
+        self.date_format = date_format
+        self.obstype = obstype
+        fp.write('#OBSCODE=%s\n' % observer_code)
