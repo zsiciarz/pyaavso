3d130ac0164378aba627b925f982a08e2f9a1560
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-28 23:11:18 +0200

    Added a basic docstring to VisualFormatWriter.

--- pyaavso/formats/visual.py
+++ pyaavso/formats/visual.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 
 class VisualFormatWriter(object):
-    pass
+    """
+    A class responsible for writing observation data in AAVSO
+    `Visual File Format`_.
+
+    .. _`Visual File Format`: http://www.aavso.org/aavso-visual-file-format
+    """
