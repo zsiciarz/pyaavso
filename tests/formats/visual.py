17390cc09c7ffd8583fd170acb2a4b1e8d4efabd
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-28 23:28:28 +0200

    Test for obscode in file.

--- tests/formats/visual.py
+++ tests/formats/visual.py
@@ -1,10 +1,20 @@
 from __future__ import unicode_literals
 
 import unittest
+from StringIO import StringIO
 
 from pyaavso.formats.visual import VisualFormatWriter
 
 
 class VisualFormatWriterTestCase(unittest.TestCase):
-    def test_writer_init(self):
-        writer = VisualFormatWriter()
+    """
+    Tests for VisualFormatWriter class.
+    """
+    def test_write_header_obscode(self):
+        """
+        Check that observer code is written into file.
+        """
+        fp = StringIO()
+        writer = VisualFormatWriter(fp, 'XYZ')
+        contents = fp.getvalue()
+        self.assertIn("#OBSCODE=XYZ", contents)
