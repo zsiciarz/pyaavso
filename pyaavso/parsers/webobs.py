27a031e84bd69c9c2f5d5da29051dc4ced6caf6a
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-01 21:17:06 +0200

    Added a dummy __init__ to WebObsResultsParser.

--- pyaavso/parsers/webobs.py
+++ pyaavso/parsers/webobs.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 
 class WebObsResultsParser(object):
-    pass
+    """
+    Parser for WebObs search results page.
+
+    The parser reads an HTML page with search results (presented as a table)
+    and parses the table into a list of observations.
+    """
+    def __init__(self, html_source):
+        """
+        Creates the parser and feeds it source code of the page.
+        """
