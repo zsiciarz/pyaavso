3b4a3aa1f0523fb49d288222c2f4e54599d6e067
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-01 21:33:44 +0200

    Initial implementation of the parser.

--- pyaavso/parsers/webobs.py
+++ pyaavso/parsers/webobs.py
@@ -16,3 +16,25 @@ class WebObsResultsParser(object):
         """
         root = html.fromstring(html_source)
         self.tbody = root.xpath('//table[@class="observations"]/tbody')[0]
+
+    def get_observations(self):
+        """
+        Parses the HTML table into a list of dictionaries, each of which
+        represents a single observation.
+        """
+        rows = list(self.tbody)
+        observations = []
+        for row_observation, row_details in zip(rows[::2], rows[1::2]):
+            data = {}
+            cells = row_observation.xpath('./td//text()[normalize-space()]')
+            data['name'] = cells[0].replace('\u2014', '').strip()
+            data['date'] = cells[1]
+            data['magnitude'] = cells[3]
+            data['obscode'] = cells[6]
+            cells = row_details.xpath('./td/div/table/tbody/tr/td//text()[normalize-space()]')
+            data['comp1'] = cells[0].replace('\u2014', '').strip()
+            data['chart'] = cells[3].replace('None', '').replace('\u2014', '').strip()
+            data['comment_code'] = cells[4].replace('\u2014', '').strip()
+            data['notes'] = cells[5].replace('\u2014', '').strip()
+            observations.append(data)
+        return observations
